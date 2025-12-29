68208e6860c83f7b1cb030d6c98645544aa8353c
Abdul Ajish <abdulajeeshsb@gmail.com>
2025-12-29 14:16:57 +0530

    provider path updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Providers } from "./provider";
+import { Providers } from "@/app/provider";
 import { NovuIntegration } from "@/components/novu/novu-integration";
 
 export const metadata: Metadata = {
